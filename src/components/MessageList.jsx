95a3e59f42c2d5103673386190c322aca0e268f8
Shiftyhead2 <shiftyhead2@gmail.com>
2022-08-17 18:23:32 +0200

    verzija 0.3.5

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -3,7 +3,7 @@ import Message from './Message';
 
 const MessageList = ({messages,user}) => {
   return (
-    <div className='text-white grid grid-rows-1 gap-1 my-1'>
+    <div className='text-white flex flex-col gap-1 my-1'>
       {messages && messages.map(msg => <Message key={msg.ID} message = {msg} user = {user} />)}
     </div>
   );
