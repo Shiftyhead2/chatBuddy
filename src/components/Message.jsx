114cfa128686cd4a5a7ff05a8ce2c25d9c4ba8d2
Shiftyhead2 <shiftyhead2@gmail.com>
2022-08-18 12:41:09 +0200

    Fixed a styling error

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -6,9 +6,9 @@ const Message = ({message, user}) => {
 
   return (
       <div>
-          <div className='inline-block  flex-col px-2 max-w-xl break-words'>
-            <p className='text-sm'>{user.uid === message.UID ? "You:" : `${message.displayName}:`}</p>
-            <div className={`rounded-xl ${messageClass} px-3 py-1`}>
+          <p className='text-base px-3'>{user.uid === message.UID ? "You:" : `${message.displayName}:`}</p>
+          <div className='inline-flex  flex-col px-3 max-w-xl break-words '>
+            <div className={`rounded-xl ${messageClass}  px-3 py-1`}>
               <p className="text-base">{message.text}</p>
             </div>
           </div>
