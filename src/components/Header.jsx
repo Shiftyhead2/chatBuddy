d561dfad01deb890d839d168553975757867876b
Shiftyhead2 <shiftyhead2@gmail.com>
2022-08-19 11:57:37 +0200

    Hopefully the final design change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({user,setUser}) => {
   return (
     <div className="header flex justify-between items-center bg-stone-800 max-h-28 py-3 fixed top-0 z-10">
         <SiGooglechat className= "text-white text-sm sm:text-2xl mx-5 md:text-3xl" />
-        <h1 className="text-green-500 text-base sm:text-3xl font-bold md:text-4xl">Chatbuddy</h1>
+        <h1 className="text-green-500  text-base sm:text-3xl font-bold md:text-4xl">Chatbuddy</h1>
         {user ? <UserDisplay user = {user} setUser = {setUser} /> : <SignIn setUser = {setUser} />}
     </div>
   );
--- src/components/UserDisplay.jsx
+++ src/components/UserDisplay.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {auth} from "../firebase";
+import {FaUserCircle} from "react-icons/fa";
 
 const UserDisplay = ({user,setUser}) => {
 
@@ -16,8 +17,11 @@ const UserDisplay = ({user,setUser}) => {
 
   return (
     <div className='text-white flex flex-col items-center mx-4 gap-2'>
-      <button className='bg-green-500 px-1.5 sm:px-3.5 md:px-6 text-xs sm:text-sm md:text-base py-1 rounded md:font-bold hover:bg-green-400 hover:text-gray-800 transition-all duration-150 ease-out'
-      onClick = {handleClick}>Sign Out</button>
+      <button className='bg-green-500 flex items-center gap-1 px-1.5 sm:px-3.5 md:px-6 text-xs sm:text-sm md:text-base py-1 rounded md:font-bold hover:bg-green-400 hover:text-gray-800 transition-all duration-150 ease-out'
+      onClick = {handleClick}>
+        <span>Sign Out</span>
+        <FaUserCircle />
+        </button>
     </div>
   );
 }
