a62c60b4f33fb94de276c5bd96cb832281aa7168
Shiftyhead2 <shiftyhead2@gmail.com>
2022-08-19 11:48:51 +0200

    Added automatic scrolling to the messages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import SignIn from "./SignIn";
 import UserDisplay from "./UserDisplay";
+import {SiGooglechat} from "react-icons/si";
 
 const Header = ({user,setUser}) => {
   return (
     <div className="header flex justify-between items-center bg-stone-800 max-h-28 py-3 fixed top-0 z-10">
-        <p className= "text-white text-xs sm:text-1xl mx-3 md:text-2xl">Chat away with:</p>
+        <SiGooglechat className= "text-white text-sm sm:text-2xl mx-5 md:text-3xl" />
         <h1 className="text-green-500 text-base sm:text-3xl font-bold md:text-4xl">Chatbuddy</h1>
         {user ? <UserDisplay user = {user} setUser = {setUser} /> : <SignIn setUser = {setUser} />}
     </div>
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React,{useEffect,useRef} from 'react';
 import Message from './Message';
 
 const MessageList = ({messages,user}) => {
+
+  const messageEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  useEffect(() => {
+    scrollToBottom();
+  },[messages]);
+
+
   return (
     <div className='text-white flex flex-col  gap-2 my-16  md:my-20  relative'>
       {messages && messages.map(msg => <Message key={msg.ID} message = {msg} user = {user} />)}
+      <div ref = {messageEndRef}></div>
     </div>
   );
 }
